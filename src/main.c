6ede2b3a4486c135e10284a4778f7e2df0f0b42c
zpifx <zpifx@proton.me>
2026-05-12 00:00:54 -0500

    simple update

--- src/main.c
+++ src/main.c
@@ -26,10 +26,42 @@ char *ReadFile(const char *fpath) {
 	return buffer;
 }
 
+int GetLine(char *largeBuffer, int lineNumber, char *StrBuffer) {
+	int nlCount = 0;
+	int charNum = 0;
+	int lineStart = 0;
+
+	char chrBuffer = 1;
+	int lineLen = 0;
+	char *subst;
+
+	while (nlCount < lineNumber) {
+		if (largeBuffer[charNum] == '\n' ) nlCount++;
+		else if (largeBuffer[charNum] == '\0') return 1;
+		charNum++;
+	}
+
+	lineStart = charNum;
+
+	while (chrBuffer != '\n') {
+		chrBuffer = largeBuffer[charNum];
+		charNum++;
+		lineLen++;
+	}
+
+	subst = largeBuffer + lineStart;
+	snprintf(StrBuffer, lineLen, subst);
+	StrBuffer[lineLen] = '\0';
+
+	return 0;
+}
 
 int main(int argc, char *argv[]) {
 	char *fileData;
-	
+	char curLine[256];
+	int i = 0;
+	int ret = 0;
+
 	if (argc < 2) {
 		printf("Too few arguments!\n");
 		exit(1);
@@ -37,7 +69,13 @@ int main(int argc, char *argv[]) {
 	
 	fileData = ReadFile(argv[1]);
 	
-	printf("%s", fileData);
+	/* printf("%s", fileData); */
+
+	while (ret == 0) {
+		ret = GetLine(fileData, i, curLine);
+		printf("%s\n", curLine);
+		i++;
+	}
 
 	return 0;
 }
